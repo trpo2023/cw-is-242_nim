4cfb527619cdbbd678c362360c283383f2f56581
root <root@DESKTOP-E559D32.localdomain>
2023-04-30 12:42:11 +0700

    Creating a Condition for Pile B

--- main.c
+++ main.c
@@ -8,3 +8,13 @@
                 printf("Ошибка: в кучке A не хватает спичек.\n");
                 continue;
             }
+	   break;
+        case 'B':
+        case 'b':
+            if (num <= b) {
+                b -= num;
+            } else {
+                printf("Ошибка: в кучке B не хватает спичек.\n");
+                continue;
+            }
+            break;
