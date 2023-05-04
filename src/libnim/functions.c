1c162f40bd7db0648bebf7cd042ab38bb526e3b7
Maximpukhov <124022126+Maximpukhov@users.noreply.github.com>
2023-05-04 13:44:51 +0700

    Update functions.c

--- src/libnim/functions.c
+++ src/libnim/functions.c
@@ -22,8 +22,7 @@ int display_heaps(int a, int b, int c)
 
 int is_game_over(int a, int b, int c)
 {
-    if (a == 0 && b == 0 && c == 0)
-    {
+    if (a == 0 && b == 0 && c == 0){
         return 1;
     }
     return 0;
@@ -31,32 +30,25 @@ int is_game_over(int a, int b, int c)
 
 int update_heap(int *a, int *b, int *c, char heap, int num)
 {
-    if (heap == 'A')
-    {
-        if (num <= 0 || num > *a)
-        {
+    if (heap == 'A'){
+        if (num <= 0 || num > *a){
             return 0;
         }
         *a -= num;
         }
-        else if (heap == 'B')
-        {
-            if (num <= 0 || num > *b)
-            {
+        else if (heap == 'B'){
+            if (num <= 0 || num > *b){
                 return 0;
             }
         *b -= num;
         }
-        else if (heap == 'C')
-        {
-            if (num <= 0 || num > *c)
-            {
+        else if (heap == 'C'){
+            if (num <= 0 || num > *c){
                 return 0;
             }
         *c -= num;
         }
-        else
-        {
+        else{
             return 0;
         }
         return 1;
